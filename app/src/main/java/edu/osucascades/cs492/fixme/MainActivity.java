861ff6004706693022ea3c1702ab2d5831a62028
mwinb <mwinberry0101@gmail.com>
2019-10-02 09:01:06 -0700

    Added Button import in MainActivity.java

--- app/src/main/java/edu/osucascades/cs492/fixme/MainActivity.java
+++ app/src/main/java/edu/osucascades/cs492/fixme/MainActivity.java
@@ -3,6 +3,7 @@ package osucascades.cs492.fixme;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class Mainactivity extends AppCompatActivity {
