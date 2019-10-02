a83bb5a398c6496f204631b4e7972b20820d5e13
mwinb <mwinberry0101@gmail.com>
2019-10-02 09:08:58 -0700

    Fixed case error with MainActivity class declaration. Was Mainactivity

--- app/src/main/java/edu/osucascades/cs492/fixme/MainActivity.java
+++ app/src/main/java/edu/osucascades/cs492/fixme/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class Mainactivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     private Button mFalseButton;
     private Button mTrueButton;
